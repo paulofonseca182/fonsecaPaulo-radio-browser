b085f611ee96851e56a49093525da060032f69b0
Paulo Fonseca <pfonseca.arq@gmail.com>
2025-02-28 16:04:11 -0300

    implementação do filtro nas radios favoritas

--- radio-browser/src/components/Favorites/Favorites.tsx
+++ radio-browser/src/components/Favorites/Favorites.tsx
@@ -1,15 +1,39 @@
-import { FavoritesPropsType } from '@/types/types';
-import React from 'react'
+import React, { useState } from "react";
+import { FavoritesPropsType } from "@/types/types";
 
-function Favorites( { favoriteRadios, toggleFavorite }: FavoritesPropsType ) {
+function Favorites({ favoriteRadios, toggleFavorite }: FavoritesPropsType) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filter, setFilter] = useState<boolean>(false);
+
+  // Filtrando as rádios com base no nome digitado
+  const filteredRadios = favoriteRadios.filter((radio) =>{
+    return (
+      radio.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      radio.country.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      radio.language.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+  );
+
   return (
     <div>
+
       <div>
-        {favoriteRadios.length === 0 ? (
+        <i onClick={() => setFilter(!filter)} className="fa fa-search"> Search</i>
+        {filter && <input
+        type="text"
+        placeholder="Search radio..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />}
+      </div>
+
+      <div>
+        {filteredRadios.length === 0 ? (
           <p>You don&apos;t have any favorite radios yet.</p>
         ) : (
           <ul>
-            {favoriteRadios.map((radio) => (
+            {filteredRadios.map((radio) => (
               <li key={radio.stationuuid}>
                 <p>{radio.name}</p>
                 <i
@@ -26,4 +50,3 @@ function Favorites( { favoriteRadios, toggleFavorite }: FavoritesPropsType ) {
 }
 
 export default Favorites;
-
