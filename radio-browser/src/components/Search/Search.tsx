9070470e8a3c6faa89a658c21f0572542a3cc02f
Paulo Fonseca <pfonseca.arq@gmail.com>
2025-02-28 08:42:35 -0300

    paginando a busca da api

--- radio-browser/src/components/Search/Search.tsx
+++ radio-browser/src/components/Search/Search.tsx
@@ -1,18 +1,42 @@
 import { fetchRadioStations } from '@/services/radioBrowserAPI'
-import React from 'react'
+import { RadioStationType } from '@/types/ApiType';
+import React, { useCallback, useEffect, useState } from 'react'
 
-const handleSearch = async () => {
-  const data = await fetchRadioStations('Brazil', undefined, 'Hunter FM - Rock HQ')
-  console.log(data);
-  
-}
 
 function Search() {
+  const [radios, setRadios] = useState<RadioStationType[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const limit = 10;
+
+
+  const handleSearch = useCallback(async () => {
+    const offset = (page - 1) * limit;
+    const data = await fetchRadioStations('Brazil', undefined, undefined, limit, offset )
+    console.log(data);
+    setRadios(data);
+  },[page])
+
+  useEffect(() => {
+    handleSearch()
+  }, [handleSearch])
+
+
   return (
+    <>
     <div>
-      <p>INICIO</p>
-      <button onClick={() => handleSearch()}>Teste</button>
+      <button onClick={() => handleSearch()}>Search</button>
     </div>
+
+    <div>
+        <button onClick={() => setPage((prev) => Math.max(prev - 1, 1))} disabled={page === 1}>
+          Previous
+        </button>
+        <span> Page {page} </span>
+        <button onClick={() => setPage((prev) => prev + 1)} disabled={radios.length < limit}>
+          Next
+        </button>
+      </div>
+    </>
   )
 }
 
--- radio-browser/src/services/radioBrowserAPI.ts
+++ radio-browser/src/services/radioBrowserAPI.ts
@@ -1,26 +1,34 @@
-import axios from 'axios';
-import { RadioStationType } from '../types/ApiType';
+import axios from "axios";
+import { RadioStationType } from "../types/ApiType";
 
 export async function fetchRadioStations(
   country?: string,
   language?: string,
   name?: string,
-  limit: number = 10
+  limit: number = 10,
+  offset: number = 0
 ): Promise<RadioStationType[]> {
   const baseUrl = process.env.NEXT_PUBLIC_RADIO_API_URL;
   if (!baseUrl) {
-    throw new Error('Environment variable is not defined');
+    throw new Error("Environment variable is not defined");
   }
 
-  const params = new URLSearchParams({ limit: String(limit) });
+  const searchParams: Record<string, string | number> = {
+    limit,
+    offset,
+  };
 
-  if (country) params.append('country', country);
-  if (language) params.append('language', language);
-  if (name) params.append('name', name);
+  if (country) searchParams["country"] = country;
+  if (language) searchParams["language"] = language;
+  if (name) searchParams["name"] = name;
+
+  const url = `${baseUrl}/json/stations/search?${new URLSearchParams(
+    Object.entries(searchParams).map(([key, value]) => [key, String(value)])
+  ).toString()}`;
 
   try {
-    const { data } = await axios.get<RadioStationType[]>(`${baseUrl}?${params}`);
-    return data;
+    const response = await axios.get<RadioStationType[]>(url);
+    return response.data;
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
       throw new Error(`Error when searching for stations: ${error.message}`);
