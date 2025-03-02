a226cebaad0f0abb88686d220abbdadaaf8640d1
Paulo Fonseca <pfonseca.arq@gmail.com>
2025-03-02 17:03:11 -0300

    page RadioBrowser renderiza em index

--- radio-browser/src/pages/index.tsx
+++ radio-browser/src/pages/index.tsx
@@ -1,50 +1,9 @@
-import Favorites from "@/components/Favorites/Favorites";
-import Search from "@/components/Search/Search";
-import { RadioStationType } from "@/types/types";
-import { useEffect, useState } from "react";
-import { loadFavorites, saveFavorites } from "@/services/localStorageService";
-import CurrentRadioPlaying from "@/components/CurrentRadioPlaying/CurrentRadioPlaying";
+import React from "react";
+import  RadioBrowser  from "@/pages/RadioBrowser/RadioBrowser";
 
 export default function Home() {
-  const [favoriteRadios, setFavoriteRadios] = useState<RadioStationType[]>([]);
-  const [currentRadioPlaying, setCurrentRadioPlaying] =
-    useState<RadioStationType>();
-
-  const toggleFavorite = (radio: RadioStationType) => {
-    setFavoriteRadios((prevFavoriteRadios) => {
-      const isFavorite = prevFavoriteRadios.some(
-        (fav) => fav.stationuuid === radio.stationuuid
-      );
-      const updatedFavorites = isFavorite
-        ? prevFavoriteRadios.filter(
-            (fav) => fav.stationuuid !== radio.stationuuid
-          )
-        : [...prevFavoriteRadios, radio];
-
-      saveFavorites(updatedFavorites);
-      return updatedFavorites;
-    });
-  };
-
-  useEffect(() => {
-    const savedFavorites = loadFavorites();
-    setFavoriteRadios(savedFavorites);
-  }, []);
 
   return (
-    <div>
-      <Search
-        favoriteRadios={favoriteRadios}
-        toggleFavorite={toggleFavorite}
-        currentRadioPlaying={currentRadioPlaying}
-        setCurrentRadioPlaying={setCurrentRadioPlaying}
-      />
-      <CurrentRadioPlaying currentRadioPlaying={currentRadioPlaying} />
-      <Favorites
-        favoriteRadios={favoriteRadios}
-        toggleFavorite={toggleFavorite}
-        setCurrentRadioPlaying={setCurrentRadioPlaying}
-      />
-    </div>
+    <RadioBrowser />
   );
 }
